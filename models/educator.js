a10874cdfcd7d3e5195f44ec15147d666b0f7920
Kelvin Graddick <kelvingraddick@kg.codes>
2022-07-11 02:32:45 -0400

    Adding isAdmin flag

--- models/educator.js
+++ models/educator.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
+  isAdmin: Boolean,
   name: { first: String, last: String },
   emailAddress: String,
   phoneNumber: String,
--- models/employer.js
+++ models/employer.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
+  isAdmin: Boolean,
   name: String,
   emailAddress: String,
   phoneNumber: String,
